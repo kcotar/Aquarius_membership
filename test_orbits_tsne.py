e35341b9ecad70548cf3fe388ddc1b0c35e8418c
kcotar <cotar.klemen@gmail.com>
2018-12-24 10:57:36 +0100

    Orbital computation changes
    - include r, R, zmax
    - faster interpolation around y=0
    - larger interpolation time, more steps

--- test_orbits_tsne.py
+++ test_orbits_tsne.py
@@ -11,30 +11,53 @@ from time import time
 from datetime import timedelta
 from astropy.table import Table
 from joblib import Parallel, delayed
+from sys import argv
+from scipy import interpolate
 
-data_dir = '/data4/cotar/'
-gaia_data = Table.read(data_dir + 'Gaia_DR2_RV/GaiaSource_combined_RV.fits')
-idx_ok = gaia_data['parallax_error']/gaia_data['parallax'] < 0.2
-idx_ok = np.logical_and(idx_ok, gaia_data['parallax'] > 0)
-idx_ok = np.logical_and(idx_ok, 1e3/gaia_data['parallax'] < 2500)
+data_dir = '/shared/ebla/cotar/'
+data_dir_out = '/shared/data-camelot/cotar/'
+gaia_data = Table.read(data_dir + 'Gaia_DR2_RV/GaiaSource_combined_RV_Bdist.fits')
+
+# remove units as they are used later in the code
+for col in gaia_data.colnames:
+    gaia_data[col].unit = None
+
+# idx_ok = gaia_data['parallax_error']/gaia_data['parallax'] < 0.2
+# idx_ok = np.logical_and(idx_ok, gaia_data['parallax'] > 0)
+# idx_ok = np.logical_and(idx_ok, 1e3/gaia_data['parallax'] < 3000)
+# gaia_data = gaia_data[idx_ok]
+
+idx_ok = gaia_data['r_est'] < 3000
 gaia_data = gaia_data[idx_ok]
 
-n_all = len(gaia_data)
-print 'Stars left:', n_all
-
-ts1 = np.linspace(0., -30., 1e4) * un.Myr
-ts2 = np.linspace(0., 30., 1e4) * un.Myr
-ts_total = np.hstack((ts1[:1:-1], ts2))
+ts1 = np.linspace(0., -45., 2e4) * un.Myr
+ts2 = np.linspace(0., 45., 2e4) * un.Myr
+ts_total = np.array(np.hstack((ts1[:1:-1], ts2)))
+n_ts_total = len(ts_total)
+n_ts_total_4 = int(n_ts_total/4)
 
 empty_vals = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
 empty_vals_actions = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
 
-
-# selected_objects = [3117420201264574080,3117533833218999552,3117654745137604608,3117293624287774720,3117405327797423488,
-#                     3117398180971951872,3117626329634066432,3117264418510436480,3117499091224753536,3117232498313708032,
-#                     3117286786699953536,3117303829129913344,3117284102343625216,3117265204484915968,3117208687015334784]
+# selected_objects = [361891062438756992]
 # gaia_data = gaia_data[np.in1d(gaia_data['source_id'], selected_objects)]
-gaia_data = gaia_data[:800000]
+
+# get arguments
+if len(argv) >= 4:
+    o_s = '_'+str(argv[3])
+    n_s = long(argv[1])
+    n_e = long(argv[2])
+else:
+    o_s = ''
+    n_s = 0
+    n_e = 6000000
+
+n_beg = len(gaia_data)
+gaia_data = gaia_data[n_s:n_e]
+
+n_all = len(gaia_data)
+print 'Stars left:', n_all, 'from initial:', n_beg
+print n_s, n_e, o_s
 
 # orbits_all = list([])
 # for id_star in range(len(gaia_data)):
@@ -49,7 +72,8 @@ def get_orbital_data(id_star):
 
     orbit1 = Orbit(vxvv=[np.float64(star_data['ra']) * un.deg,
                          np.float64(star_data['dec']) * un.deg,
-                         1e3 / np.float64(star_data['parallax']) * un.pc,
+                         # 1e3 / np.float64(star_data['parallax']) * un.pc,
+                         np.float64(star_data['r_est']) * un.pc,
                          np.float64(star_data['pmra']) * un.mas / un.yr,
                          np.float64(star_data['pmdec']) * un.mas / un.yr,
                          np.float64(star_data['rv']) * un.km / un.s],
@@ -59,7 +83,8 @@ def get_orbital_data(id_star):
 
     orbit2 = Orbit(vxvv=[np.float64(star_data['ra']) * un.deg,
                          np.float64(star_data['dec']) * un.deg,
-                         1e3 / np.float64(star_data['parallax']) * un.pc,
+                         # 1e3 / np.float64(star_data['parallax']) * un.pc,
+                         np.float64(star_data['r_est']) * un.pc,
                          np.float64(star_data['pmra']) * un.mas / un.yr,
                          np.float64(star_data['pmdec']) * un.mas / un.yr,
                          np.float64(star_data['rv']) * un.km / un.s],
@@ -71,48 +96,70 @@ def get_orbital_data(id_star):
     orbit2.turn_physical_on()
 
     orbit1.integrate(ts1, MWPotential2014)
-
     orbit2.integrate(ts2, MWPotential2014)
 
     orbit_y = np.hstack((orbit1.y(ts1)[:1:-1], orbit2.y(ts2)))
 
-    try:
-        ts_get = np.interp(0., orbit_y, ts_total) * un.Myr
-    except:
-        print 'Unable to determine ts_get'
-        orbit_xyz = empty_vals
+    i_cross = np.nanargmin(np.abs(orbit_y - 0.))
+    i_cross_s = max(i_cross - 100, 0)
+    i_cross_e = min(i_cross + 100, n_ts_total-1)
+    mean_y = np.abs(np.mean(orbit_y[i_cross_s:i_cross_e]))
+    # print mean_y, i_cross_s, i_cross_e
 
-    try:
-        if ts_get < 0.:
-            orbit_xyz = [orbit1.x(ts_get) * 1e3, orbit1.y(ts_get) * 1e3, orbit1.z(ts_get) * 1e3,  # in pc
-                         orbit1.vx(ts_get), orbit1.vy(ts_get), orbit1.vz(ts_get),
-                         orbit1.U(ts_get), orbit1.V(ts_get), orbit1.W(ts_get)]  # in km/s
-        else:
-            orbit_xyz = [orbit2.x(ts_get) * 1e3, orbit2.y(ts_get) * 1e3, orbit2.z(ts_get) * 1e3,
-                         orbit2.vx(ts_get), orbit2.vy(ts_get), orbit2.vz(ts_get),
-                         orbit2.U(ts_get), orbit2.V(ts_get), orbit2.W(ts_get)]
-        if np.abs(orbit_xyz[1]) > 1e-7:
-            print 'Orbit not crossing Y=0'
+    if mean_y < 0.1:
+        ts_get = interpolate.interp1d(orbit_y[i_cross_s:i_cross_e],
+                                      ts_total[i_cross_s:i_cross_e],
+                                      bounds_error=False, fill_value=np.nan, assume_sorted=False)(0.) * un.Myr
+    else:
+        print 'Unable to determine ts_get (mean_y = '+str(mean_y)+') -> '+str(star_data['source_id'])
+        ts_get = np.nan
+
+    # plt.plot(orbit_y, ts_total)
+    # plt.plot(orbit_y[i_cross_s:i_cross_e], ts_total[i_cross_s:i_cross_e])
+    # plt.axvline(0.)
+    # try:
+    #     plt.axhline(ts_get.value)
+    # except:
+    #     pass
+    # plt.show()
+    # plt.close()
+    # print ts_get
+
+    if np.isfinite(ts_get):
+        try:
+            if ts_get < 0.:
+                orbit_xyz = [orbit1.x(ts_get) * 1e3, orbit1.y(ts_get) * 1e3, orbit1.z(ts_get) * 1e3,  # in pc
+                             orbit1.vx(ts_get), orbit1.vy(ts_get), orbit1.vz(ts_get),  # in km/s
+                             orbit1.U(ts_get), orbit1.V(ts_get), orbit1.W(ts_get)]  # in km/s
+            else:
+                orbit_xyz = [orbit2.x(ts_get) * 1e3, orbit2.y(ts_get) * 1e3, orbit2.z(ts_get) * 1e3,
+                             orbit2.vx(ts_get), orbit2.vy(ts_get), orbit2.vz(ts_get),
+                             orbit2.U(ts_get), orbit2.V(ts_get), orbit2.W(ts_get)]
+            if np.abs(orbit_xyz[1]) > 1e-5:
+                print 'Orbit not crossing Y=0 -> '+str(star_data['source_id'])
+                orbit_xyz = empty_vals
+            else:
+                orbit_xyz.append(orbit2.e(analytic=True, pot=MWPotential2014))
+                orbit_xyz.append(orbit2.zmax(analytic=True, pot=MWPotential2014))
+                orbit_xyz.append(orbit2.rap(analytic=True, pot=MWPotential2014))
+                orbit_xyz.append(orbit2.rperi(analytic=True, pot=MWPotential2014))
+        except:
+            print 'Error in orbit ts_get -> '+str(star_data['source_id'])
             orbit_xyz = empty_vals
-        else:
-            orbit_xyz.append(orbit2.e(analytic=True, pot=MWPotential2014))
-            orbit_xyz.append(orbit2.zmax(analytic=True, pot=MWPotential2014))
-            orbit_xyz.append(orbit2.rap(analytic=True, pot=MWPotential2014))
-            orbit_xyz.append(orbit2.rperi(analytic=True, pot=MWPotential2014))
-    except:
-        print 'Error in orbit ts_get'
-        orbit_xyz = empty_vals
 
-    try:
-        orbit2.turn_physical_off()
-        # Staeckel model
-        delta_s = estimateDeltaStaeckel(MWPotential2014, orbit2.R(ts2), orbit2.z(ts2))
-        action_staeckel = actionAngleStaeckel(pot=MWPotential2014, delta=delta_s, c=True)
-        # actions, frequencies and angles
-        object_actions = action_staeckel.actionsFreqsAngles(orbit2.R(), orbit2.vR(), orbit2.vT(), orbit2.z(),
-                                                            orbit2.vz(), orbit2.phi(), fixed_quad=True)
-    except:
-        print 'Error in orbit action estimation'
+        try:
+            orbit2.turn_physical_off()
+            # Staeckel model
+            delta_s = estimateDeltaStaeckel(MWPotential2014, orbit2.R(ts2), orbit2.z(ts2))
+            action_staeckel = actionAngleStaeckel(pot=MWPotential2014, delta=delta_s, c=True)
+            # actions, frequencies and angles
+            object_actions = action_staeckel.actionsFreqsAngles(orbit2.R(), orbit2.vR(), orbit2.vT(), orbit2.z(),
+                                                                orbit2.vz(), orbit2.phi(), fixed_quad=True)
+        except:
+            print 'Error in orbit action estimation -> '+str(star_data['source_id'])
+            object_actions = empty_vals_actions
+    else:
+        orbit_xyz = empty_vals
         object_actions = empty_vals_actions
 
     for o_v in orbit_xyz:
@@ -122,18 +169,16 @@ def get_orbital_data(id_star):
 
     return orbit_vals_all
 
-
 t_s = time()
-orbits_all = Parallel(n_jobs=35)(delayed(get_orbital_data)(i_row) for i_row in range(len(gaia_data)))
+orbits_all = Parallel(n_jobs=18)(delayed(get_orbital_data)(i_row) for i_row in range(len(gaia_data)))
 
 # print orbits_all
 orbits_all = np.vstack(orbits_all)
 # print orbits_all
 
-d_out = gaia_data['source_id', 'ra', 'dec', 'pmra', 'pmra_error', 'pmdec_error', 'parallax', 'parallax_error', 'rv', 'rv_error']
+d_out = gaia_data['source_id', 'ra', 'dec', 'pmra', 'pmra_error', 'pmdec_error', 'parallax', 'parallax_error', 'rv', 'rv_error', 'r_est', 'r_lo', 'r_hi']
 
 for i_c, c_new in enumerate(['X', 'Y', 'Z', 'VX', 'VY', 'VZ', 'U', 'V', 'W', 'e', 'zmax', 'Rap', 'Rper', 'J_R', 'L_Z', 'J_Z', 'Omega_R', 'Omega_Phi', 'Omega_Z', 'Theta_R', 'Theta_Phi', 'Theta_Z']):
     d_out[c_new] = orbits_all[:, i_c]
 
-out_dir = '/data4/cotar/'
-d_out.write(out_dir+'Gaia_dr2_orbital_derivatives_actions_1.fits', overwrite=True)
+d_out.write(data_dir_out+'Gaia_dr2_orbital_derivatives_actions_Bdist'+o_s+'.fits', overwrite=True)
--- visualize_orbits_test.py
+++ visualize_orbits_test.py
@@ -12,15 +12,18 @@ from time import time
 from astropy.table import Table, join
 
 data_dir = '/data4/cotar/'
-gaia_data = Table.read(data_dir + 'Gaia_DR2_RV/GaiaSource_combined_RV.fits')
+gaia_data = Table.read(data_dir + 'Gaia_DR2_RV/GaiaSource_combined_RV_Bdist.fits')
 
 # clusters = Table.read(data_dir+'Open_cluster_members_DR2.fits')
 # selected_objects = clusters[clusters['cluster'] == 'ASCC_105']['source_id']
 
-selected_objects = [1584855752702115584,1585161142056865792,1585581013763892608,1599170638182153344,1601407388430279552,1606891889869191296,1608412789327913088,1613431437137809792,1622667270386650880,1626000302447696512,1626626577398964864,1627096996577394816,1627813190963893888,1633652967801335168,1635701980734926336,1636102958881605632,1637192952862070144,1642683840915751936,1647154425129739136,1647550730351839232,1647901783798855680,1648224078143868672,1662578850984726272,1662715503959596032,1677492970300907648,1680114760073513984,1680691419562252160,1681675963505181184,1682129615130992256,1686846588733634304,1690210510199277184,1691646090132093824,1695631961517553280,1695939687333571712,1696437899244881920,1697407844595341568,1703781266825101440,1707783209976268928,1708072317110964864,1709881735293952384,1712976070251310720,1713053860699038080,1718579078426160128]
+selected_objects = [429968076498071424,552471646265579648,223202479644072576,302204142444316160,507514986631702656,
+                    283866246999592576,429607544063400704,570143134586746496,452619051118116224,429471440832635776,
+                    466280689250024064,405595928089960320,421499985836042496,415660956313404800,479765477806636032,
+                    420200427506635136,431282787468706560,240491269198173568,330024466686913792,158265834005992960]
 # selected_objects = [2007911710694905728,2162765176002359168,2171423009746515840,2202700782658624000]
 # selected_objects = [3017250126427057024,3018575033640452736,3121604220965378944,3208961454882028416,3210151053446429056,3210775438611713024,4496352578733523584,4508720160407113984,4518269899935842176,4577886588615102592]
-# selected_objects = [5970493171281327872,5970751453436281856,5971299220726443136,5971459989970403968,6020604685025643648,6030906146908479360]
+# selected_objects = [5970493171281327872,5970751453436281856,5971299220726443136,5974526190511181162241459989970403968,6020604685025643648,6030906146908479360]
 gaia_subset = gaia_data[np.in1d(gaia_data['source_id'], selected_objects)]
 
 # gaia_data = gaia_data[gaia_data['parallax_error']/gaia_data['parallax'] < 0.2]
@@ -29,8 +32,8 @@ gaia_subset = gaia_data[np.in1d(gaia_data['source_id'], selected_objects)]
 # gaia_subset = gaia_subset[np.int64(np.random.choice(np.arange(len(gaia_subset)), 250, replace=False))]
 
 ts0 = 0. * un.Myr
-ts1 = np.linspace(0., -50., 1e3) * un.Myr
-ts2 = np.linspace(0., 50., 1e3) * un.Myr
+ts1 = np.linspace(0., -30., 5e4) * un.Myr
+ts2 = np.linspace(0., 30., 5e4) * un.Myr
 
 orbit_sun = Orbit(vxvv=[0. * un.deg,
                         0. * un.deg,
@@ -49,7 +52,8 @@ for star_data in gaia_subset:
 
     orbit = Orbit(vxvv=[np.float64(star_data['ra']) * un.deg,
                         np.float64(star_data['dec']) * un.deg,
-                        1e3 / np.float64(star_data['parallax']) * un.pc,
+                        # 1e3 / np.float64(star_data['parallax']) * un.pc,
+                        np.float64(star_data['r_est']) * un.pc,
                         np.float64(star_data['pmra']) * un.mas / un.yr,
                         np.float64(star_data['pmdec']) * un.mas / un.yr,
                         np.float64(star_data['rv']) * un.km / un.s],
@@ -70,13 +74,47 @@ for star_data in gaia_subset:
                  np.hstack((orbit_xyz1[2][:1:-1], orbit_xyz2[2])),
                  np.hstack((orbit_xyz1[3][:1:-1], orbit_xyz2[3]))]
 
-    ax[0, 0].plot(orbit_xyz[0], orbit_xyz[1], lw=0.8)
+    ax[0, 0].plot(orbit_xyz[0], orbit_xyz[1], lw=0.8, c='C0')
     ax[0, 0].scatter(orbit_ts0[0], orbit_ts0[1], lw=0, s=4, c='black')
-    ax[1, 0].plot(orbit_xyz[0], orbit_xyz[2], lw=0.8)
+    ax[1, 0].plot(orbit_xyz[0], orbit_xyz[2], lw=0.8, c='C0')
     ax[1, 0].scatter(orbit_ts0[0], orbit_ts0[2], lw=0, s=4, c='black')
-    ax[0, 1].plot(orbit_xyz[2], orbit_xyz[1], lw=0.8)
+    ax[0, 1].plot(orbit_xyz[2], orbit_xyz[1], lw=0.8, c='C0')
     ax[0, 1].scatter(orbit_ts0[2], orbit_ts0[1], lw=0, s=4, c='black')
-    ax[1, 1].plot(orbit_xyz[3], orbit_xyz[2], lw=0.8)
+    ax[1, 1].plot(orbit_xyz[3], orbit_xyz[2], lw=0.8, c='C0')
+    ax[1, 1].scatter(orbit_ts0[3], orbit_ts0[2], lw=0, s=4, c='black')
+
+
+    orbit = Orbit(vxvv=[np.float64(star_data['ra']) * un.deg,
+                        np.float64(star_data['dec']) * un.deg,
+                        1e3 / np.float64(star_data['parallax']) * un.pc,
+                        # np.float64(star_data['r_est']) * un.pc,
+                        np.float64(star_data['pmra']) * un.mas / un.yr,
+                        np.float64(star_data['pmdec']) * un.mas / un.yr,
+                        np.float64(star_data['rv']) * un.km / un.s],
+                  radec=True,
+                  ro=8.2, vo=238., zo=0.025,
+                  solarmotion=[-11., 10., 7.25])  # as used by JBH in his paper on forced oscillations and phase mixing
+    orbit.turn_physical_on()
+
+    orbit.integrate(ts1, MWPotential2014)
+    orbit_ts0 = [orbit.x(ts0) * 1e3, orbit.y(ts0) * 1e3, orbit.z(ts0) * 1e3, orbit.R(ts0) * 1e3]
+    orbit_xyz1 = [orbit.x(ts1) * 1e3, orbit.y(ts1) * 1e3, orbit.z(ts1) * 1e3, orbit.R(ts1) * 1e3]
+
+    orbit.integrate(ts2, MWPotential2014)
+    orbit_xyz2 = [orbit.x(ts2) * 1e3, orbit.y(ts2) * 1e3, orbit.z(ts2) * 1e3, orbit.R(ts2) * 1e3]
+
+    orbit_xyz = [np.hstack((orbit_xyz1[0][:1:-1], orbit_xyz2[0])),
+                 np.hstack((orbit_xyz1[1][:1:-1], orbit_xyz2[1])),
+                 np.hstack((orbit_xyz1[2][:1:-1], orbit_xyz2[2])),
+                 np.hstack((orbit_xyz1[3][:1:-1], orbit_xyz2[3]))]
+
+    ax[0, 0].plot(orbit_xyz[0], orbit_xyz[1], lw=0.8, c='C3')
+    ax[0, 0].scatter(orbit_ts0[0], orbit_ts0[1], lw=0, s=4, c='black')
+    ax[1, 0].plot(orbit_xyz[0], orbit_xyz[2], lw=0.8, c='C3')
+    ax[1, 0].scatter(orbit_ts0[0], orbit_ts0[2], lw=0, s=4, c='black')
+    ax[0, 1].plot(orbit_xyz[2], orbit_xyz[1], lw=0.8, c='C3')
+    ax[0, 1].scatter(orbit_ts0[2], orbit_ts0[1], lw=0, s=4, c='black')
+    ax[1, 1].plot(orbit_xyz[3], orbit_xyz[2], lw=0.8, c='C3')
     ax[1, 1].scatter(orbit_ts0[3], orbit_ts0[2], lw=0, s=4, c='black')
 
 
